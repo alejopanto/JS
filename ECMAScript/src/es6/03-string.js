5756d7b84f3bf72a6c8af17b3ba6a41d94fa1d7d
alejopanto <jose.pantoja@testu.co>
2024-12-20 08:50:09 -0500

    string (template-literals)

---
+++ ECMAScript/src/es6/03-string.js
@@ -0,0 +1,15 @@
+let hello = "Hello";
+let world = "World";
+let epicPhrase = hello + ' ' + world + '!';
+
+// Template literals (backticks)
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2);
+
+// Multi-line strings
+let lorem = "Esto es un string \n" + "esto es otra linea";
+let lorem2 = `Esto es una frase epica
+    la continuacion de esa frase epica
+`
+console.log(lorem);
+console.log(lorem2);
