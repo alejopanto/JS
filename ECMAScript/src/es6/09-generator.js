b4746dc85bb2faf43a15b57f26c423379112e300
alejopanto <jose.pantoja@testu.co>
2025-01-16 15:17:14 -0500

    set-add

--- ECMAScript/src/es6/09-generator.js
+++ ECMAScript/src/es6/09-generator.js
@@ -10,15 +10,3 @@ console.log(it.next().value);
 console.log(it.next().value);
 console.log(it.next().value);
 console.log(it.next().value);
-
-function* getId(michis) {
-  for (let index = 0; index < michis.length; index++) {
-    yield index+1
-  }
-}
-
-const michis = getId(['a', 'b', 'c'])
-console.log(michis.next().value);
-console.log(michis.next().value);
-console.log(michis.next().value);
-console.log(michis.next().value);
